8eabae9c02e9a2e7806bb9cb16ffdf5137dfe9ba
Ieva <kerseviciute.ieva@gmail.com>
2025-03-19 13:34:37 +0100

    Abstract neuron class.

--- model_neuron.py
+++ model_neuron.py
@@ -1,59 +1,77 @@
 from neuron import h
 import os
+from abc import ABC, abstractmethod
 
-class Neuron:
-    def __init__(self, morphology):
-        self.is_created = False
-        self.morphology = morphology
 
-        self.hoc_dir = os.getcwd()
+class Neuron(ABC):
+    """
+    The abstract neuron class encapsulates all steps to initialize
+    the model neuron. This includes loading the necessary files,
+    inserting mechanisms, and defining init() for resetting the neuron.
+
+    See the extensions of this class for models with different synapse
+    types. Use only those models to run the simulations.
+
+    NOTE: only one neuron per python session can be created.
+    """
+
+    # TODO: would it be possible to define the neuron in such a way
+    # TODO: that it is separate from h? (now these are equivalent)
+    # TODO: would be useful to run simulations in parallel
+    def __init__(self):
+        self.__is_created = False
+        self.__hoc_dir = os.getcwd()
 
         self.create_cell()
 
     def create_cell(self):
         # Only create the cell if it is not yet created
-        if self.is_created: return
+        if self.__is_created:
+            print("Cell already created")
+            return
+
+        print("Creating cell")
 
         # Load standard run tools
         h.load_file("stdrun.hoc")
 
         # Load cell anatomical and biophysical properties
-        self.load_hoc("cellspec_c62564.hoc")
-        self.load_hoc("biophys.hoc")
-        self.load_hoc("fluct.hoc")
+        self.load_hoc("nrnhoc/cellspec_c62564.hoc")
+        self.load_hoc("nrnhoc/biophys.hoc")
+        self.load_hoc("nrnhoc/fluct.hoc")
 
         self.insert_mechanism("extracellular")
         self.insert_mechanism("xtrau")
 
         # Only interpolates sections that have xtrau
-        self.load_hoc("interpxyzu.hoc")
+        self.load_hoc("nrnhoc/interpxyzu.hoc")
 
-        self.load_hoc("setpointersu.hoc")
+        self.load_hoc("nrnhoc/setpointersu.hoc")
 
         # Computes scale factor used to calculate extracellular potential
         # produced by a uniform electrical field
-        self.load_hoc("calcrxcu.hoc")
+        self.load_hoc("nrnhoc/calcrxcu.hoc")
 
         # Extracellular stimulus
-        self.load_hoc("zapstimu2.hoc")
+        self.load_hoc("nrnhoc/zapstimu2.hoc")
 
-        h('proc init() { nrnpython("Neuron.neuron_init()") }')
+        h('proc init() { nrnpython("Neuron.__neuron_init()") }')
 
-        self.is_created = True
+        self.initialize()
 
+        self.__is_created = True
+
+    # noinspection PyMethodMayBeStatic
     def insert_mechanism(self, mechanism):
         for sec in h.allsec():
             sec.insert(mechanism)
 
     def load_hoc(self, hoc_file):
-        hoc_path = os.path.join(self.hoc_dir, hoc_file)
+        hoc_path = os.path.join(self.__hoc_dir, hoc_file)
         h.load_file(hoc_path)
 
-    def set_stimulus(self, delay, duration, freq1, freq2, amplitude, phase):
-        h.setstim(delay, duration, freq1, freq2, amplitude, phase)
-
     @staticmethod
-    def neuron_init():
+    def __neuron_init():
         h.t = 0
 
         for section in h.allsec():
@@ -84,7 +102,8 @@ class Neuron:
             h.fcurrent()
 
         for section in h.allsec():
-            if h.ismembrane("na3", sec = section) or h.ismembrane("nax", sec = section):
+            if h.ismembrane("na3", sec = section) or \
+               h.ismembrane("nax", sec = section):
                 for segment in section:
                     # Calculate passive current for sodium channels
                     segment.e_pas = segment.v + (segment.ina + segment.ik) / segment.g_pas
@@ -95,3 +114,11 @@ class Neuron:
                     segment.e_pas = segment.e_pas + segment.i_hd / segment.g_pas
 
         print("Neuron initialized")
+
+    """
+        initialize() is called during create_cell(). Use this method to include
+        synapses, set up stimulation parameters, etc.
+    """
+    @abstractmethod
+    def initialize(self):
+        pass
