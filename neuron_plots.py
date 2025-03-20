b40b3b9140556d2b43814a36c6d67115b03393e1
Ieva Kerseviciute <42313252+kerseviciute@users.noreply.github.com>
2025-03-20 14:27:51 +0100

    Refactoring code and working on LTP protocol (#3)
    * Neuron conda env.
    
    * Some neuron commands, ignore compiled.
    
    * Updated code from Rosanna.
    
    * Reformat.
    
    * Update parameters in GUI on change.
    
    * Main simulation file.
    
    * Visualization of temporal interference frequencies.
    
    * Remove random noise.
    
    * Add a single synapse.
    
    * Add a bunch of synapses at random locations.
    
    * Save and plot synapse stats.
    
    * Neuron behavior in an oscillating electric field without synapses.
    
    * Neuron behavior in an oscillating electric field with random input.
    
    * Plot orientation of the electric field.
    
    * Update env.
    
    * More accurate approximation of the electric field strength to elicit APs.
    
    * Model effect of the direction of electric field.
    
    * Move some unnecessary files into a separate dir.
    
    * Add NMDA and AMPA synapses and follow weights.
    
    * Clean up code.
    
    * Raster plot of in-vivo stimulation.
    
    * Working on simulations.
    
    * Reformat h.mod, include parameter for ehd.
    
    * Move file.
    
    * Bug fix.
    
    * Define neuron init.
    
    * Remove GUI components from ZAP stim.
    
    * Abstract neuron class.
    
    * Plastic neuron with NMDA and AMPA synapses.
    
    * Clean up file.
    
    * Move notebooks and .mod files.
    
    * Read synapse information.
    
    * Rename class.
    
    * Refactor synapses into a separate class. Set initial synapse weight.
    
    * Simplify neuron initialization.
    
    * Keep track of main parameters in the synapse.
    
    * Plot neuron and synapses.
    
    * Refactoring. Working on synapse parameter tuning.
    
    * Pass stimuli and delays as functions.
    
    * Hide GUI components. Make sure EF works.
    
    * LTP protocol.
    
    * Save final weights.

---
+++ neuron_plots.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+class NeuronPlots:
+    @staticmethod
+    def plot_all_synapses(neuron):
+        fig, axs = plt.subplots(nrows = 2, ncols = 2, figsize = (10, 10))
+        axs = axs.flatten()
+
+        for synapse in neuron.synapses:
+            axs[0].plot(neuron.time, synapse.weights, linewidth = 0.5)
+
+        axs[0].set_ylim(-0.1, 1.1)
+        axs[0].set_title("Synaptic weights")
+
+        for synapse in neuron.synapses:
+            axs[1].plot(neuron.time, synapse.current, linewidth = 0.5)
+
+        axs[1].set_title("Synapse current")
+
+        for synapse in neuron.synapses:
+            axs[2].plot(neuron.time, synapse.conductance, linewidth = 0.5)
+
+        axs[2].set_title("Synapse conductance")
+
+        for synapse in neuron.synapses:
+            axs[3].plot(neuron.time, synapse.voltage, linewidth = 0.5)
+
+        axs[3].set_title("Synapse EPSP")
+
+    @staticmethod
+    def plot_soma_voltage(neuron, scaled = True):
+        plt.plot(neuron.time, neuron.voltage, linewidth = 0.5, color = "black")
+        plt.xlabel("Time, ms")
+        plt.ylabel("Voltage, mV")
+
+        if scaled:
+            plt.ylim(-100, 70)
+
+    @staticmethod
+    def plot_spike_frequency(neuron):
+        inst_freq, time_points = neuron.spike_frequency
+
+        fig, axs = plt.subplots(nrows = 2, ncols = 1, figsize = (8, 5))
+
+        axs[0].plot(neuron.time, neuron.voltage, linewidth = 0.5, color = "black")
+        axs[0].set_xlabel("Time (ms)")
+        axs[0].set_ylabel("Voltage (mV)")
+        axs[0].set_xlim(np.min(neuron.time), np.max(neuron.time))
+        axs[0].set_ylim(-100, 70)
+
+        axs[1].scatter(time_points, inst_freq, s = 3, color = "black")
+        axs[1].set_xlabel("Time (ms)")
+        axs[1].set_ylabel("Frequency (Hz)")
+        axs[1].set_xlim(np.min(neuron.time), np.max(neuron.time))
+
+        if len(inst_freq) > 0:
+            axs[1].set_ylim(-1, np.max(inst_freq) + 5)
+
+        fig.tight_layout()
+
+    @staticmethod
+    def plot_synaptic_weights(neuron):
+        for synapse in neuron.synapses:
+            plt.plot(neuron.time, synapse.weights)
+
+        plt.ylim(-0.1, 1.1)
+        plt.title("Synaptic weights")
+        plt.xlabel("Time, ms")
+        plt.ylabel("Synaptic weight")
---
+++ neuron_utils.py
@@ -0,0 +1,68 @@
+from neuron import h
+import random
+
+
+class NeuronUtils:
+    @staticmethod
+    def count_section(section_name):
+        return sum([ section_name in section.name() for section in h.allsec() ])
+
+    @staticmethod
+    def generate_synapse_location(n_apical_dendrites, max_distance, min_distance):
+        """
+        Generates a random synapse location within the allowed distance bounds.
+
+        :param n_apical_dendrites: number of apical dendrites
+        :param max_distance: maximum distance to soma
+        :param min_distance: minimum distance to soma
+        :return: dendrite id, position along the dendrite, and distance to the soma
+        """
+
+        distance_to_soma = max_distance + 1
+        dendrite_idx = -1
+        loc_pos = -1.0
+
+        while distance_to_soma > max_distance or distance_to_soma < min_distance:
+            dendrite_idx = random.randint(0, n_apical_dendrites - 1)
+            loc_pos = random.uniform(0, 1)
+            dendrite = h.apical_dendrite[dendrite_idx]
+
+            distance_to_soma = h.distance(dendrite(loc_pos))
+
+        return dendrite_idx, loc_pos, distance_to_soma
+
+    @staticmethod
+    def create_burst_stimulus():
+        stimulus = h.BurstStim3()
+        stimulus.interval = 25
+        stimulus.noise = 0
+        stimulus.burstlen = 100
+        stimulus.burstint = 250 - stimulus.burstlen
+
+        return stimulus
+
+    @staticmethod
+    def create_constant_freq_stimulus():
+        frequency = 50
+        noise = 0
+        delay = 0
+
+        stimulus = h.NetStim()
+        stimulus.interval = int(1000 / frequency)
+        stimulus.noise = noise
+        stimulus.start = delay
+
+        return stimulus
+
+    @staticmethod
+    def create_no_stimulus():
+        return None
+
+    @staticmethod
+    def set_stimulus(delay = 100, duration = 900, frequency1 = 1000, frequency2 = 1005, amplitude = 100, phase = 0):
+        h.setstim(delay, duration, frequency1, frequency2, amplitude, phase)
+
+    @staticmethod
+    def set_field(theta_deg = 90, phi_deg = 90):
+        field_length = 100
+        h.changefield(field_length, theta_deg, phi_deg)
